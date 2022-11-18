64e67fc3355d8263995888987981852fee08d508
Philipp <philer7@gmail.com>
2022-11-18 15:38:49 +0700

    Task 8 is done once again :)

---
+++ Seminar1_C#_HW/Задача_8/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.WriteLine("To see all even numbers starting from 1 to N, enter N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+while (current <= number)
+{
+    if (current % 2 == 0)
+    Console.Write($"{current}, ");
+    current++;
+}
